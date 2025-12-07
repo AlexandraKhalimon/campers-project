f9a59403f7e555a48c8e3078bd6e785b8df343ce
Alexandra Khalimon <lexikhalimon@gmail.com>
2025-12-07 19:35:59 +0200

    add catalog folder and update Hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,7 +1,11 @@
+"use client"
+
 import css from "./Hero.module.css";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Hero() {
+    const router = useRouter();
     return (
         <section>
             <div className={css.hero}>
@@ -16,7 +20,7 @@ export default function Hero() {
                         <h1 className={css.title}>Campers of your dreams</h1>
                         <p className={css.text}>You can find everything you want in our catalog</p>
                     </div>
-                    <button className={css.button}>View Now</button>
+                    <button className={css.button} onClick={()=>router.push("/catalog")}>View Now</button>
                 </div>
             </div>
         </section>
